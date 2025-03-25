26147a87f4fa4255a09582cf38e14f58744bc098
SofiaMartinez23 <sofiamg2392001@gmail.com>
2025-03-25 09:12:39 +0100

    Add project files.

---
+++ MvcCoreEmpleadosMultioplesRutas/Controllers/EmpleadosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEmpleadosMultioplesRutas.Services;
+using NuetApiModelsSMG.Models;
+
+namespace MvcCoreEmpleadosMultioplesRutas.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ServiceEmpleados service;
+
+        public EmpleadosController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Empleado> empleados = await this.service.GetEmpleadosAsync();
+            List<string> oficios = await this.service.GetOficiosAsync();
+
+            ViewData["OFICIOS"] = oficios;
+            return View(empleados);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string oficio)
+        {
+            List<Empleado> oficioEmp = await this.service.GetEmpleadosOficioAsync(oficio);
+            List<string> oficios = await this.service.GetOficiosAsync();
+
+            ViewData["OFICIOS"] = oficios;
+
+            return View(oficioEmp);
+        }
+
+    }
+}
---
+++ MvcCoreEmpleadosMultioplesRutas/Services/ServiceEmpleados.cs
@@ -0,0 +1,65 @@
+﻿using NuetApiModelsSMG.Models;
+using System.Net.Http.Headers;
+
+namespace MvcCoreEmpleadosMultioplesRutas.Services
+{
+    public class ServiceEmpleados
+    {
+        private string ApiUrl;
+
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceEmpleados(IConfiguration configuration)
+        {
+            this.ApiUrl = configuration.GetValue<string>("ApiUrls:ApiEmpleados");
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            string request = "api/empleados";
+            List<Empleado> data = await this.CallApiAsync<List<Empleado>>(request); 
+            return data;
+            
+        }
+
+        public async Task<List<string>> GetOficiosAsync()
+        {
+            string request = "api/empleados/oficios";
+            List<string> data = await this.CallApiAsync<List<string>>(request);
+            return data;
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosOficioAsync(string oficio)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/empleados/empleadosoficio/" + oficio;
+                List<Empleado> data = await this.CallApiAsync<List<Empleado>>(request);
+                return data;
+
+            }
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.ApiUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.GetAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();  
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+    }
+}
